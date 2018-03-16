167a2b9aa368975baf096e641d8ee8798fd82ec7
gilsunshine <gilsunshine@gmail.com>
2018-03-16 13:47:56 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -3,4 +3,26 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
 
+  let index = 0;
+
+  document.addEventListener('keydown', onKeyDownHandler)
+
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+
+    if (key === code[index]) {
+      index++;
+      console.log(index)
+      console.log(key)
+
+      if (index === code.length) {
+        console.log('hello')
+        alert("Hurray!")
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
 }
